3bf15914954b0591c02c29901255527c7941a80a
afolabi <yusufafolabi95@gmail.com>
2024-07-26 19:09:15 +0100

    Integrate Paystack payment gateway

--- utils/paymentUtils.js
+++ utils/paymentUtils.js
@@ -5,15 +5,20 @@ require('dotenv').config();
 
 const router = express.Router();
 
-const MONNIFY_BASE_URL = process.env.MONNIFY_BASE_URL;
-const MONNIFY_API_KEY = process.env.MONNIFY_API_KEY;
-const MONNIFY_SECRET_KEY = process.env.MONNIFY_SECRET_KEY;
-const MONNIFY_CONTRACT_CODE = process.env.MONNIFY_CONTRACT_CODE;
+// const MONNIFY_BASE_URL = process.env.MONNIFY_BASE_URL;
+// const MONNIFY_API_KEY = process.env.MONNIFY_API_KEY;
+// const MONNIFY_SECRET_KEY = process.env.MONNIFY_SECRET_KEY;
+// const MONNIFY_CONTRACT_CODE = process.env.MONNIFY_CONTRACT_CODE;
 
 const DEV_REDIRECT_URL = process.env.DEV_REDIRECT_URL;
 const PROD_REDIRECT_URL = process.env.PROD_REDIRECT_URL;
 
 const REDIRECT_URL = process.env.NODE_ENV === 'production' ? PROD_REDIRECT_URL : DEV_REDIRECT_URL;
+
+const PAYSTACK_BASE_URL = process.env.PAYSTACK_BASE_URL;
+const PAYSTACK_SECRET_KEY = process.env.PAYSTACK_SECRET_KEY;
+
+
 
 // Helper function to get auth token
 const getAuthToken = async () => {
@@ -44,6 +49,99 @@ function generateRandompayRef(length) {
     return paymentRef;
 }
 
+// Route to initialize a transaction For Monify
+// router.post('/initialize-transaction/:id', async (req, res) => {
+//     try {
+//         const { customerName, customerEmail } = req.body;
+
+//         // Check for required fields
+//         if (!customerName || !customerEmail) {
+//             return res.status(400).json({ error: 'customerName and customerEmail are required fields' });
+//         }
+
+//         // Generate a random payment reference
+//         const paymentRef = generateRandompayRef(10);
+
+//         const authToken = await getAuthToken();
+
+//         const response = await axios.post(`${MONNIFY_BASE_URL}/merchant/transactions/init-transaction`, {
+//             amount: 1000.0,
+//             customerName,
+//             customerEmail,
+//             paymentReference: paymentRef,
+//             paymentDescription: "Jamb Focus",
+//             currencyCode: "NGN",
+//             contractCode: MONNIFY_CONTRACT_CODE,
+//             redirectUrl: `${REDIRECT_URL}/${req.params.id}`, 
+//             paymentMethods: ["CARD", "ACCOUNT_TRANSFER"]
+//         }, {
+//             headers: {
+//                 Authorization: `Bearer ${authToken}`
+//             }
+//         });
+
+//         if (response.data.requestSuccessful) {
+//             res.status(200).json(response.data.responseBody.checkoutUrl);
+//         } else {
+//             console.error('Monnify error response:', response.data);
+//             res.status(500).json({ error: response.data.responseMessage || 'An error occurred' });
+//         }
+//     } catch (error) {
+//         console.error('Error initializing transaction:', error.response ? error.response.data : error.message);
+//         res.status(500).json({ error: 'An error occurred while initializing the transaction' });
+//     }
+// });
+
+
+
+// Route to handle the redirect after payment For Monify
+// router.get('/payment-response/:id', async (req, res) => {
+//     const { paymentReference } = req.query;
+
+//     if (!paymentReference) {
+//         return res.status(400).json({ error: 'Payment reference required' });
+//     }
+
+//     try {
+//         const user = await User.findById(req.params.id);
+
+//         if (!user) {
+//             return res.status(404).json({ error: 'User not found' });
+//         }
+
+//         const authToken = await getAuthToken();
+
+//         const response = await axios.get(`${MONNIFY_BASE_URL}/merchant/transactions/query?paymentReference=${paymentReference}`, {
+//             headers: {
+//                 Authorization: `Bearer ${authToken}`
+//             }
+//         });
+
+//         if (response.data.requestSuccessful) {
+//             const paymentStatus = response.data.responseBody.paymentStatus;
+
+//             if (paymentStatus === 'PAID') {
+//                 user.profile.membership = 'Premium';
+//                 await user.save();
+
+//                 res.redirect('https://www.jambfocus.com/admin/profile');
+//             } else {
+//                 res.status(500).redirect('https://www.jambfocus.com/admin/profile');
+//             }
+//         } else {
+//             console.error('Monnify error response:', response.data);
+//             res.status(500).json({ error: response.data.responseMessage || 'An error occurred' });
+//         }
+//     } catch (error) {
+//         console.error('Error handling payment response:', error.response ? error.response.data : error.message);
+//         res.status(500).json({ error: 'An error occurred while processing the payment response' });
+//     }
+// });
+
+
+
+
+
 // Route to initialize a transaction
 router.post('/initialize-transaction/:id', async (req, res) => {
     try {
@@ -57,29 +155,26 @@ router.post('/initialize-transaction/:id', async (req, res) => {
         // Generate a random payment reference
         const paymentRef = generateRandompayRef(10);
 
-        const authToken = await getAuthToken();
-
-        const response = await axios.post(`${MONNIFY_BASE_URL}/merchant/transactions/init-transaction`, {
-            amount: 1000.0,
-            customerName,
-            customerEmail,
-            paymentReference: paymentRef,
-            paymentDescription: "Jamb Focus",
-            currencyCode: "NGN",
-            contractCode: MONNIFY_CONTRACT_CODE,
-            redirectUrl: `${REDIRECT_URL}/${req.params.id}`, 
-            paymentMethods: ["CARD", "ACCOUNT_TRANSFER"]
+        const response = await axios.post(`${PAYSTACK_BASE_URL}/transaction/initialize`, {
+            email: customerEmail,
+            amount: 10000, 
+            reference: paymentRef,
+            callback_url: `${REDIRECT_URL}/${req.params.id}`,
+            metadata: {
+                customerName,
+                paymentDescription: "Jamb Focus"
+            }
         }, {
             headers: {
-                Authorization: `Bearer ${authToken}`
+                Authorization: `Bearer ${PAYSTACK_SECRET_KEY}`
             }
         });
 
-        if (response.data.requestSuccessful) {
-            res.status(200).json(response.data.responseBody.checkoutUrl);
+        if (response.data.status) {
+            res.status(200).json(response.data.data.authorization_url);
         } else {
-            console.error('Monnify error response:', response.data);
-            res.status(500).json({ error: response.data.responseMessage || 'An error occurred' });
+            console.error('Paystack error response:', response.data);
+            res.status(500).json({ error: response.data.message || 'An error occurred' });
         }
     } catch (error) {
         console.error('Error initializing transaction:', error.response ? error.response.data : error.message);
@@ -88,12 +183,11 @@ router.post('/initialize-transaction/:id', async (req, res) => {
 });
 
 
-
 // Route to handle the redirect after payment
 router.get('/payment-response/:id', async (req, res) => {
-    const { paymentReference } = req.query;
+    const { reference } = req.query;
 
-    if (!paymentReference) {
+    if (!reference) {
         return res.status(400).json({ error: 'Payment reference required' });
     }
 
@@ -104,18 +198,16 @@ router.get('/payment-response/:id', async (req, res) => {
             return res.status(404).json({ error: 'User not found' });
         }
 
-        const authToken = await getAuthToken();
-
-        const response = await axios.get(`${MONNIFY_BASE_URL}/merchant/transactions/query?paymentReference=${paymentReference}`, {
+        const response = await axios.get(`${PAYSTACK_BASE_URL}/transaction/verify/${reference}`, {
             headers: {
-                Authorization: `Bearer ${authToken}`
+                Authorization: `Bearer ${PAYSTACK_SECRET_KEY}`
             }
         });
 
-        if (response.data.requestSuccessful) {
-            const paymentStatus = response.data.responseBody.paymentStatus;
+        if (response.data.status) {
+            const paymentStatus = response.data.data.status;
 
-            if (paymentStatus === 'PAID') {
+            if (paymentStatus === 'success') {
                 user.profile.membership = 'Premium';
                 await user.save();
 
@@ -124,50 +216,13 @@ router.get('/payment-response/:id', async (req, res) => {
                 res.status(500).redirect('https://www.jambfocus.com/admin/profile');
             }
         } else {
-            console.error('Monnify error response:', response.data);
-            res.status(500).json({ error: response.data.responseMessage || 'An error occurred' });
+            console.error('Paystack error response:', response.data);
+            res.status(500).json({ error: response.data.message || 'An error occurred' });
         }
     } catch (error) {
         console.error('Error handling payment response:', error.response ? error.response.data : error.message);
         res.status(500).json({ error: 'An error occurred while processing the payment response' });
     }
 });
-
-
-
-
-
-
-
-
-
-
-
-// router.get('/payment-response/:id', async (req, res) => {
-//     const { paymentReference } = req.query;
-
-//     if (!paymentReference ) {
-//         return res.status(400).json({ error: 'Payment reference required' });
-//     }
-
-//     try {
-//         const user = await User.findById(req.params.id);
-
-//         if (!user) {
-//             return res.status(404).json({ error: 'User not found' });
-//         }
-
-//         if (paymentStatus === 'success') {
-//             user.profile.membership = 'premium';
-//             await user.save();
-//             res.json({ message: 'Payment successful', membership: user.profile.membership });
-//         } else {
-//             res.send('Payment failed');
-//         }
-//     } catch (error) {
-//         console.error('Error handling payment response:', error);
-//         res.status(500).json({ error: 'An error occurred while processing the payment response' });
-//     }
-// });
 
 module.exports = router;
